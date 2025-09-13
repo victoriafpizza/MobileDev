396dcbb8c99b69117268d433e648ed17840401bf
victoriafpizza <rm550609@fiap.com.br>
2025-09-12 22:19:46 -0300

    configurando axios

---
+++ AppPostApi/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+// Import React {useEffect, useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+export default function App() {
+
+type Post = {
+  title: string;
+  body: string;
+}
+
+//O componente é o ponto de entrada da aplicação 
+const App = () => {
+  const [postMessage, setPost] = useState<Post | null>(null);
+}
+
+//UseEffect é um hook que permite executar o efeito colateral em um componetne
+//o hook recebe o dado e realiza alguma função com ele.
+useEffect (() => { //problema do react
+  axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => setPost (response.data)); //agora o type script entende o tipo que ta retornando
+
+}, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
